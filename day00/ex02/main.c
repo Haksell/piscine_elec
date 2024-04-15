08253f3d976d29d375251f68702593ce96735d69
Axel Brisse <axel.brisse@gmail.com>
2024-04-15 12:48:20 +0200

    is_released

--- day00/ex02/main.c
+++ day00/ex02/main.c
@@ -1,5 +1,3 @@
-// https://www.youtube.com/watch?v=YR4I0G_ILk4
-
 #include <avr/io.h>
 #include <stdbool.h>
 #include <util/delay.h>
@@ -7,8 +5,8 @@
 int main() {
     DDRB = 1 << PB0; // Configure pin PB0 as output
     while (true) {
-        bool is_pressed = ((PIND >> PD2) & 1) == 0; // Check state of PD2 (0 if pressed)
-        PORTB = is_pressed ? 1 << PB0 : 0; // Set the pin PB0 to on or off
+        bool is_released = (PIND >> PD2) & 1; // Check state of PD2
+        PORTB = is_released ? 0 : 1 << PB0; // Set the pin PB0 to on or off
         _delay_ms(20);
     }
     return 0;
