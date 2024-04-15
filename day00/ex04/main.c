8deba3535ec49e332fb589090dfdf7c47afc853c
Axel Brisse <axel.brisse@gmail.com>
2024-04-15 14:31:52 +0200

    avoid debounce overflow

--- day00/ex04/main.c
+++ day00/ex04/main.c
@@ -2,16 +2,20 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#define DEBOUNCE_ITERATIONS 5
+#define DEBOUNCE_ITERATIONS 20
 
 int main() {
-    DDRB = 0b10111; // Configure pins PB0, Pb1, PB2, PB4 as output
+    DDRB = 0b10111; // Configure pins PB0, PB1, PB2, PB4 as output
     int n = 0; // Value to display
     int counterSW1 = 0;
     int counterSW2 = 0;
     while (true) {
-        counterSW1 = (PIND >> PD2) & 1 ? 0 : counterSW1 + 1;
-        counterSW2 = (PIND >> PD4) & 1 ? 0 : counterSW2 + 1;
+        counterSW1 = (PIND >> PD2) & 1                  ? 0
+                     : counterSW1 > DEBOUNCE_ITERATIONS ? counterSW1
+                                                        : counterSW1 + 1;
+        counterSW2 = (PIND >> PD4) & 1                  ? 0
+                     : counterSW2 > DEBOUNCE_ITERATIONS ? counterSW2
+                                                        : counterSW2 + 1;
         if (counterSW1 == DEBOUNCE_ITERATIONS) ++n;
         if (counterSW2 == DEBOUNCE_ITERATIONS) --n;
         int portb = 0;
@@ -20,7 +24,7 @@ int main() {
         if (n & 4) portb |= 1 << PB2;
         if (n & 8) portb |= 1 << PB4;
         PORTB = portb;
-        _delay_ms(10);
+        _delay_ms(1);
     }
     return 0;
 }
