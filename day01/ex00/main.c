d6246407502068909811e521957c6fd776bb911f
Axel Brisse <axel.brisse@gmail.com>
2024-04-15 23:25:25 +0200

    day01/ex00

---
+++ day01/ex00/main.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <stdbool.h>
+#include <util/delay.h>
+
+#define LED (1 << PB1)
+
+int main() {
+    DDRB = LED;
+    while (true) {
+        PORTB ^= LED;
+        _delay_ms(500);
+    }
+    return 0;
+}
