e63d43faf99f8423c4882648cec2fdb491d9c570
Axel Brisse <axel.brisse@gmail.com>
2024-04-16 21:48:17 +0200

    day01/ex02 clean

--- day01/ex02/main.c
+++ day01/ex02/main.c
@@ -6,17 +6,16 @@
 
 #define DUTY_CYCLE 0.1
 
-static void set_wgm_mode() {
-    TCCR1A |= 1 << WGM11;
-    TCCR1B |= 1 << WGM12 | 1 << WGM13;
-    ICR1 = F_CPU / 1024 - 1; // top value before reset to 0
-    OCR1A = ICR1 * DUTY_CYCLE;
-    TCCR1A |= 1 << COM1A1; // light up when timer1 < OCR1A
-}
-
 int main() {
     DDRB = 1 << PB1; // setup LED D2 as output
     TCCR1B |= 1 << CS10 | 1 << CS12; // prescaler = 1024
-    set_wgm_mode();
+
+    // Count up to ICR1
+    TCCR1A |= 1 << WGM11;
+    TCCR1B |= 1 << WGM12 | 1 << WGM13;
+
+    ICR1 = F_CPU / 1024 - 1; // top value before reset to 0
+    OCR1A = ICR1 * DUTY_CYCLE;
+    TCCR1A |= 1 << COM1A1; // light up when timer1 < OCR1A
     while (true) {}
 }
