ee5442375daa5bd9eb3c094926c8d239012092fc
Axel Brisse <axel.brisse@gmail.com>
2024-04-16 02:13:15 +0200

    day01/ex02 ok this is good stop

--- day01/ex02/main.c
+++ day01/ex02/main.c
@@ -15,7 +15,7 @@ static void set_fast_pwm_mode() {
 }
 
 int main() {
-    DDRB = (1 << PB1);
+    DDRB = (1 << PB1); // setup LED D2 as output
     TCCR1A = 0;
     TCCR1B = 1 << CS10 | 1 << CS12; // prescaler = 1024
     set_fast_pwm_mode();
