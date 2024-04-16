3ad946def5b160fd5d2ac947fcad2bf5955259a2
Axel Brisse <axel.brisse@gmail.com>
2024-04-16 14:16:54 +0200

    clean day00/ex04

--- day00/ex04/main.c
+++ day00/ex04/main.c
@@ -6,20 +6,15 @@
 
 int main() {
     DDRB = 0b10111; // Configure pins PB0, PB1, PB2, PB4 as output
-    int n = 0; // Value to display
-    int counterSW1 = 0;
-    int counterSW2 = 0;
+    uint8_t n = 0; // Value to display
+    uint8_t counterSW1 = 0;
+    uint8_t counterSW2 = 0;
     while (true) {
         counterSW1 = (PIND >> PD2) & 1 ? 0 : counterSW1 + (counterSW1 <= DEBOUNCE_ITERATIONS);
         counterSW2 = (PIND >> PD4) & 1 ? 0 : counterSW2 + (counterSW2 <= DEBOUNCE_ITERATIONS);
         if (counterSW1 == DEBOUNCE_ITERATIONS) ++n;
         if (counterSW2 == DEBOUNCE_ITERATIONS) --n;
-        int portb = 0;
-        if (n & 1) portb |= 1 << PB0;
-        if (n & 2) portb |= 1 << PB1;
-        if (n & 4) portb |= 1 << PB2;
-        if (n & 8) portb |= 1 << PB4;
-        PORTB = portb;
+        PORTB = (n & 7) | (n & 8) << 1;
         _delay_ms(1);
     }
 }
