8be89333d20e1e06017fd7cc9c5a1c6ca280f706
Axel Brisse <axel.brisse@gmail.com>
2024-04-16 15:26:54 +0200

    day01/ex00 no delay

--- day01/ex00/main.c
+++ day01/ex00/main.c
@@ -5,9 +5,14 @@
 #define LED (1 << PB1)
 
 int main() {
+    const uint32_t LOOP_CYCLES = 14;
+    const uint32_t RESET_COUNTER = F_CPU / LOOP_CYCLES / 2 - 1;
     DDRB = LED;
+    uint32_t i = 0;
     while (true) {
-        PORTB ^= LED;
-        _delay_ms(500);
+        if (i == RESET_COUNTER) {
+            PORTB ^= LED;
+            i = 0;
+        } else ++i;
     }
 }
