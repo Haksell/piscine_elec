67d2c21cc7288ad5f884abcc2cb3e7f3f5d0596e
Axel Brisse <axel.brisse@gmail.com>
2024-04-17 11:07:51 +0200

    wip

--- day02/ex00/main.c
+++ day02/ex00/main.c
@@ -3,22 +3,23 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#define DEBOUNCE_ITERATIONS 12
-
-volatile uint8_t counter = 13; // Start value to prove output works. TODO: set at 0
+volatile uint8_t counter = 0; // Start value to prove output works. TODO: set at 0
 
 int main() {
     sei();
 
     DDRB = 0b10111;
     EIMSK = 1 << INT0;
-    PCMSK2 = 1 << PCINT18;
-    EICRA = 0b00;
+    EICRA = 0b10;
 
     while (true) {
         PORTB = (counter & 7) | (counter & 8) << 1;
-        _delay_ms(1);
+        _delay_ms(10);
     }
 }
 
-ISR(INT0_vect) { ++counter; }
+ISR(INT0_vect) {
+    EICRA ^= 0b11;
+    // millis to debounce?
+    ++counter;
+}
