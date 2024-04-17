e57ed05689f3756adaec278f98ff06f0d14d256f
Axel Brisse <axel.brisse@gmail.com>
2024-04-17 23:53:41 +0200

    day02/ex01 good except hardcoded OCRA1_STEP

--- day02/ex01/main.c
+++ day02/ex01/main.c
@@ -3,30 +3,31 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#define PWM_FREQ 1000
-#define OOGA 0.0001
+#define OCRA1_STEP 0.000064 // slightly too fast
+#define IRC1_RESET 999
 
 volatile float dutyCycle = 0;
 bool increasing = true;
 
 ISR(TIMER0_COMPA_vect) {
     if (increasing) {
-        dutyCycle += OOGA;
+        dutyCycle += OCRA1_STEP;
         if (dutyCycle >= 1) {
             dutyCycle = 1;
             increasing = false;
         }
     } else {
-        dutyCycle -= OOGA;
+        dutyCycle -= OCRA1_STEP;
         if (dutyCycle <= 0) {
             dutyCycle = 0;
             increasing = true;
         }
     }
-    OCR1A = dutyCycle * 0x3ff;
+    OCR1A = dutyCycle * IRC1_RESET;
 }
 
 void setupTimer0() {
+    // Normal node, no prescaling
     TCCR0A = 0;
     TCCR0B = (TCCR0B & 0b11111000) | (1 << CS00);
     TIMSK0 |= 1 << OCIE0A;
@@ -34,9 +35,11 @@ void setupTimer0() {
 
 void setupTimer1() {
     DDRB |= 1 << PB1;
-    TCCR1A |= 1 << WGM10 | 1 << WGM11 | 1 << COM1A1;
-    TCCR1B |= 1 << WGM12 | 1 << CS10;
     OCR1A = 0;
+    // Light OC1A on when 0 <= TCNT1 < OCR1A, off when OCR1A <= TCNT1 < ICR1
+    TCCR1A |= 1 << WGM11 | 1 << COM1A1;
+    ICR1 = IRC1_RESET;
+    TCCR1B |= 1 << WGM12 | 1 << WGM13 | 1 << CS10;
 }
 
 int main() {
