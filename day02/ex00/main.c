456512ef739d3e14d41062b2b7437b47ea249ac3
Axel Brisse <axel.brisse@gmail.com>
2024-04-17 22:03:07 +0200

    with debounce counter

--- day02/ex00/main.c
+++ day02/ex00/main.c
@@ -3,11 +3,14 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-#define DEBOUNCE_DELAY 20
 #define LED (1 << PB0)
 #define PIN (1 << PD2)
 
-volatile bool led_is_on = false;
+#define DEBOUNCE_ITERATIONS 12
+
+volatile bool button_is_pressed = false;
+
+uint8_t min(uint8_t a, uint8_t b) { return a < b ? a : b; }
 
 int main() {
     DDRB = LED;
@@ -15,19 +18,19 @@ int main() {
     PORTD |= PIN;
 
     EIMSK |= 1 << INT0;
-    EICRA = 0b00;
+    EICRA = 0b10;
 
     sei();
 
+    uint8_t counter = 0;
     while (true) {
-        PORTB = led_is_on ? LED : 0;
-        _delay_ms(DEBOUNCE_DELAY);
-        EIMSK |= 1 << INT0;
+        counter = button_is_pressed ? min(counter, DEBOUNCE_ITERATIONS) + 1 : 0;
+        if (counter == DEBOUNCE_ITERATIONS) PORTB ^= LED;
+        _delay_ms(1);
     }
 }
 
 ISR(INT0_vect) {
-    if (EICRA == 0b00) led_is_on = !led_is_on;
-    EICRA ^= 0b11;
-    EIMSK &= ~(1 << INT0);
+    button_is_pressed = EICRA == 0b10;
+    EICRA ^= 0b01;
 }
