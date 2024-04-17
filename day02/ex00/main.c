c7a98557034e6c99a50e381a15f4f7a931142619
Axel Brisse <axel.brisse@gmail.com>
2024-04-17 10:51:21 +0200

    still kinda works

--- day02/ex00/main.c
+++ day02/ex00/main.c
@@ -3,22 +3,21 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
+#define DEBOUNCE_ITERATIONS 12
+
 volatile uint8_t counter = 13; // Start value to prove output works. TODO: set at 0
 
 int main() {
     sei();
 
     DDRB = 0b10111;
-    DDRD &= ~(1 << PD2);
-    PORTD |= 1 << PORTD2;
-    EIMSK |= 1 << INT0;
+    EIMSK = 1 << INT0;
     PCMSK2 = 1 << PCINT18;
     EICRA = 0b00;
-    EIFR |= (1 << INTF0);
 
     while (true) {
         PORTB = (counter & 7) | (counter & 8) << 1;
-        _delay_ms(10);
+        _delay_ms(1);
     }
 }
 
