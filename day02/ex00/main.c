4779054d26e7ce6819f536f602428df2cc834bad
Axel Brisse <axel.brisse@gmail.com>
2024-04-17 11:29:05 +0200

    works but ugly

--- day02/ex00/main.c
+++ day02/ex00/main.c
@@ -3,23 +3,22 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-volatile uint8_t counter = 0; // Start value to prove output works. TODO: set at 0
+volatile bool led_is_on = false;
 
 int main() {
-    sei();
-
-    DDRB = 0b10111;
+    DDRB = 1 << PB0;
     EIMSK = 1 << INT0;
     EICRA = 0b10;
 
+    sei();
+
     while (true) {
-        PORTB = (counter & 7) | (counter & 8) << 1;
+        PORTB = led_is_on ? 1 << PB0 : 0;
         _delay_ms(10);
     }
 }
 
 ISR(INT0_vect) {
+    if (EICRA == 0b10) led_is_on = !led_is_on;
     EICRA ^= 0b11;
-    // millis to debounce?
-    ++counter;
 }
