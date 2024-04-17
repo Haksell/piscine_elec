e8de642d3a0e795977bc85eaa6eff41ef9be872e
Axel Brisse <axel.brisse@gmail.com>
2024-04-17 21:36:18 +0200

    works but ugly

--- day02/ex00/main.c
+++ day02/ex00/main.c
@@ -3,22 +3,31 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
+volatile bool button_is_pressed = false;
 volatile bool led_is_on = false;
 
 int main() {
     DDRB = 1 << PB0;
     EIMSK = 1 << INT0;
-    EICRA = 0b10;
+    EICRA = 0b00;
 
     sei();
 
     while (true) {
+        EIMSK |= 1 << INT0;
         PORTB = led_is_on ? 1 << PB0 : 0;
-        _delay_ms(10);
+        _delay_ms(20);
     }
 }
 
 ISR(INT0_vect) {
-    if (EICRA == 0b10) led_is_on = !led_is_on;
-    EICRA ^= 0b11;
+    if (EICRA == 0b00) {
+        button_is_pressed = true;
+        led_is_on = !led_is_on;
+        EICRA = 0b11;
+    } else {
+        button_is_pressed = false;
+        EICRA = 0b00;
+    }
+    EIMSK &= ~(1 << INT0);
 }
