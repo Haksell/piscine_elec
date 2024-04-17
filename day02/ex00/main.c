8f2462dc78cc3093f8eea425207588a86537b6c9
Axel Brisse <axel.brisse@gmail.com>
2024-04-17 09:45:05 +0200

    .

--- day02/ex00/main.c
+++ day02/ex00/main.c
@@ -6,14 +6,12 @@
 #define DEBOUNCE_ITERATIONS 20
 
 int main() {
-    DDRB = 1 << PB0; // Configure pin PB0 as output
+    DDRB = 1 << PB0;
     int counter = 0;
     while (true) {
-        bool is_released = (PIND >> PD2) & 1; // Check state of PD2
+        bool is_released = (PIND >> PD2) & 1;
         counter = is_released ? 0 : counter > DEBOUNCE_ITERATIONS ? counter : counter + 1;
-        if (counter == DEBOUNCE_ITERATIONS) {
-            PORTB ^= 1 << PB0; // Switch the state of the LED
-        }
+        if (counter == DEBOUNCE_ITERATIONS) { PORTB ^= 1 << PB0; }
         _delay_ms(1);
     }
 }
