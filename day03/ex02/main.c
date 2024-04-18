79217a13b096df3efca215f2b583666292b7d50a
Axel Brisse <axel.brisse@gmail.com>
2024-04-18 04:19:52 +0200

    day03/ex02 good (screen is retarded)

---
+++ day03/ex02/main.c
@@ -0,0 +1,29 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdbool.h>
+
+#define ROUND_DIV(dividend, divisor)                                                               \
+    ({                                                                                             \
+        typeof(divisor) _divisor = (divisor);                                                      \
+        ((dividend) + (_divisor >> 1)) / _divisor;                                                 \
+    })
+
+static void uart_init(void) {
+    UBRR0 = ROUND_DIV(F_CPU, 16 * UART_BAUDRATE) - 1;
+    UCSR0B |= 1 << RXEN0 | 1 << TXEN0;
+}
+
+static void uart_tx(char c) {
+    while (!(UCSR0A & 1 << UDRE0)) {}
+    UDR0 = c;
+}
+
+static char uart_rx() {
+    while (!(UCSR0A & 1 << RXC0)) {}
+    return UDR0;
+}
+
+int main() {
+    uart_init();
+    while (true) uart_tx(uart_rx());
+}
