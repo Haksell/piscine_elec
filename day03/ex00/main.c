984a832959858526b6094caa1664d1c8b8c6a87d
Axel Brisse <axel.brisse@gmail.com>
2024-04-18 02:55:19 +0200

    .

--- day03/ex00/main.c
+++ day03/ex00/main.c
@@ -4,15 +4,15 @@
 #include <util/delay.h>
 
 static void uart_init(void) {
-    UBRR0 = F_CPU / 8 / UART_BAUDRATE - 1; // Set baud rate register
-    UCSR0A |= 1 << U2X0; // Enable 2x speed
-    UCSR0B |= 1 << TXEN0; // Enable transmitter
-    // We could set 8N1 format in UCSR0B but it seems to be the default already
+    UBRR0 = F_CPU / 8 / UART_BAUDRATE - 1; // set baud rate register
+    UCSR0A |= 1 << U2X0; // enable 2x speed
+    UCSR0B |= 1 << TXEN0; // enable transmitter
+    // we could set 8N1 format in UCSR0B but it seems to be the default already
 }
 
 static void uart_tx(char c) {
-    while (!(UCSR0A & 1 << UDRE0)) {} // Wait for the transmit buffer to be empty
-    UDR0 = c; // Put data into the buffer
+    while (!(UCSR0A & 1 << UDRE0)) {} // wait for the transmit buffer to be empty
+    UDR0 = c; // put data into the buffer
 }
 
 int main() {
