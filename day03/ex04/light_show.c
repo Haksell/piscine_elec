887a020f39e0cd15767d92832071ca893c6cfce3
Axel Brisse <axel.brisse@gmail.com>
2024-04-18 07:16:27 +0200

    day03/ex04 gray code

--- day03/ex04/light_show.c
+++ day03/ex04/light_show.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
+#define DELAY_MS 50
+
 void light_show() {
     DDRB = 0b10111;
     DDRD = 0b1101000;
+    PORTB = PORTD = 0;
     uint8_t n = 0;
     while (true) {
-        PORTB = (n & 7) | (n & 8) << 1;
-        PORTD = (n & 3) << 5 | (n & 4) << 1;
         ++n;
-        _delay_ms(100);
+        _delay_ms(DELAY_MS);
+        PORTB = (n & 7) | (n & 8) << 1;
+        _delay_ms(DELAY_MS);
+        uint8_t l3b = n & 7;
+        if (n & 1) PORTD ^= 1 << 6;
+        else if (n & 2) PORTD ^= 1 << 5;
+        else PORTD ^= 1 << 3;
     }
 }
