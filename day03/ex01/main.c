95b0b8759ad59716233b3307fb43bbdfe8a6515d
Axel Brisse <axel.brisse@gmail.com>
2024-04-18 03:29:03 +0200

    day03/ex01 good except loop is definitely not empty

---
+++ day03/ex01/main.c
@@ -0,0 +1,39 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdbool.h>
+#include <util/delay.h>
+
+#define ROUND_DIV(dividend, divisor)                                                               \
+    ({                                                                                             \
+        typeof(divisor) _divisor = (divisor);                                                      \
+        ((dividend) + (_divisor >> 1)) / _divisor;                                                 \
+    })
+
+static void uart_init(void) {
+    UBRR0 = ROUND_DIV(F_CPU, 16 * UART_BAUDRATE) - 1; // set baud rate register
+    UCSR0B |= 1 << TXEN0; // enable transmitter
+    // we could set 8N1 format in UCSR0B but it seems to be the default already
+}
+
+static void uart_tx(char c) {
+    while (!(UCSR0A & 1 << UDRE0)) {} // wait for the transmit buffer to be empty
+    UDR0 = c; // put data into the buffer
+}
+
+static void uart_printstr(const char* str) {
+    while (*str) uart_tx(*str++);
+}
+
+static void uart_printstrln(const char* str) {
+    uart_printstr(str);
+    uart_tx('\r');
+    uart_tx('\n');
+}
+
+int main() {
+    uart_init();
+    while (true) {
+        uart_printstrln("Hello World!");
+        _delay_ms(2000);
+    }
+}
