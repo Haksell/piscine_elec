be373d342055c30b1d9f0e2ac6d147bce9594923
Axel Brisse <axel.brisse@gmail.com>
2024-04-20 14:50:14 +0200

    remove parentheses

--- day04/ex02/i2c.c
+++ day04/ex02/i2c.c
@@ -3,23 +3,23 @@
 void i2c_init() { TWBR = (F_CPU / I2C_F - 16) / 2; }
 
 void i2c_start() {
-    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTA);
+    TWCR = 1 << TWINT | 1 << TWEN | 1 << TWSTA;
     while (!(TWCR & 1 << TWINT)) {}
 }
 
 void i2c_stop() {
-    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+    TWCR = 1 << TWINT | 1 << TWEN | 1 << TWSTO;
     while (!(TWCR & 1 << TWSTO)) {}
 }
 
 void i2c_write(uint8_t byte) {
     TWDR = byte;
-    TWCR = (1 << TWINT) | (1 << TWEN);
+    TWCR = 1 << TWINT | 1 << TWEN;
     while (!(TWCR & 1 << TWINT)) {}
 }
 
 uint8_t i2c_read(uint8_t ack) {
-    TWCR = (1 << TWINT) | (1 << TWEN) | (!!ack << TWEA);
+    TWCR = 1 << TWINT | 1 << TWEN | !!ack << TWEA;
     while (!(TWCR & 1 << TWINT)) {}
     return TWDR;
 }
