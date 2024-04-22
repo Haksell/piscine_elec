9a4be04f496b1ceda8a886a2ede46b3ce6b54f13
Axel Brisse <axel.brisse@gmail.com>
2024-04-22 10:40:22 +0200

    day05/ex00 ok

--- day05/ex00/main.c
+++ day05/ex00/main.c
@@ -2,18 +2,20 @@
 #include <avr/io.h>
 #include <stdbool.h>
 
-#define DEBOUNCE_ITERATIONS 10
+#define DEBOUNCE_ITERATIONS 100
 #define COUNTER_ADDR ((uint8_t*)0x42)
 
 int main() {
-    DDRB = 0b10111;
-    eeprom_write_byte(COUNTER_ADDR, 0);
+    DDRB |= 0b10111;
+    DDRD &= ~(1 << PD2);
+    // eeprom_write_byte(COUNTER_ADDR, 0);
     int8_t counterSW1 = -1;
     while (true) {
         counterSW1 = (PIND >> PD2) & 1 ? DEBOUNCE_ITERATIONS : counterSW1 - (counterSW1 >= 0);
-        uint8_t n = eeprom_read_byte(COUNTER_ADDR);
-        if (counterSW1 == 0) ++n;
-        PORTB = (n & 7) | (n & 8) << 1;
-        eeprom_write_byte(COUNTER_ADDR, n);
+        if (counterSW1 == 0) {
+            uint8_t n = eeprom_read_byte(COUNTER_ADDR) + 1;
+            PORTB = (n & 7) | (n & 8) << 1;
+            eeprom_write_byte(COUNTER_ADDR, n);
+        }
     }
 }
