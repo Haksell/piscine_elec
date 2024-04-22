c0626e7c0e1e66974d4135250711839ef67b4334
Axel Brisse <axel.brisse@gmail.com>
2024-04-22 11:14:31 +0200

    day05/ex01 ok

--- day05/ex01/main.c
+++ day05/ex01/main.c
@@ -10,6 +10,11 @@
 
 static void leds(uint8_t n) { PORTB = (n & 7) | (n & 8) << 1; }
 
+static bool is_pressed(uint8_t pin, int8_t* debounce) {
+    *debounce = (PIND >> pin) & 1 ? DEBOUNCE_ITERATIONS : *debounce - (*debounce >= 0);
+    return *debounce == 0;
+}
+
 int main() {
     DDRB = 0b10111;
     DDRD &= ~(1 << PD2 | 1 << PD4);
@@ -17,15 +22,13 @@ int main() {
     int8_t sw1 = -1;
     int8_t sw2 = -1;
     while (true) {
-        sw1 = (PIND >> PD2) & 1 ? DEBOUNCE_ITERATIONS : sw1 - (sw1 >= 0);
-        sw2 = (PIND >> PD4) & 1 ? DEBOUNCE_ITERATIONS : sw2 - (sw2 >= 0);
-        if (sw1 == 0) {
+        if (is_pressed(PD2, &sw1)) {
             uint8_t* addr = COUNTER_ADDR(eeprom_read_byte(COUNTERS_ADDR));
             uint8_t n = eeprom_read_byte(addr) + 1;
             eeprom_write_byte(addr, n);
             leds(n);
         }
-        if (sw2 == 0) {
+        if (is_pressed(PD4, &sw2)) {
             uint8_t selected = (eeprom_read_byte(COUNTERS_ADDR) + 1) % NUM_COUNTERS;
             eeprom_write_byte(COUNTERS_ADDR, selected);
             leds(eeprom_read_byte(COUNTER_ADDR(selected)));
