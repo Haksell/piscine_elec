fed63e94b8e551d0479e67e1d0e4887bec6ca4ea
Axel Brisse <axel.brisse@gmail.com>
2024-04-22 18:09:29 +0200

    day05/ex04 pretty bad wip honestly

---
+++ day05/ex04/main.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <avr/eeprom.h>
+#include <avr/io.h>
+#include <stdbool.h>
+
+#define ROUND_DIV(dividend, divisor)                                                               \
+    ({                                                                                             \
+        typeof(divisor) _divisor = (divisor);                                                      \
+        ((dividend) + (_divisor >> 1)) / _divisor;                                                 \
+    })
+
+#include "uart.h"
