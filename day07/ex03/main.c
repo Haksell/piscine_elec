4d4451502b3c63668f74782b084d1a90141c7181
Axel Brisse <axel.brisse@gmail.com>
2024-04-24 02:32:39 +0200

    day07/ex03 good except random constants

---
+++ day07/ex03/main.c
@@ -0,0 +1,49 @@
+#include "uart.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <math.h> // TODO: remove?
+#include <stdbool.h>
+
+#define CTC_MODE (1 << WGM12)
+#define TIMER_PRESCALER_256 (1 << CS12)
+#define PRINT_INTERVAL 0.02
+#define ADC_THERMISTOR PC2
+
+ISR(ADC_vect) { ADCSRA |= 1 << ADSC; }
+
+static void adc_init() {
+    ADMUX = 1 << REFS0 | PC2;
+    ADCSRA = 1 << ADPS2 | 1 << ADPS1 | 1 << ADPS0 | 1 << ADIE | 1 << ADEN | 1 << ADSC;
+}
+
+#define R_BALANCE 10000.0
+#define V_REF 5.0
+#define T_REF 298.15 // TODO
+#define R_REF 10000.0 // TODO
+#define BETA_COEFFICIENT 3950.0 // TODO
+
+static float convert_adc_to_celsius(uint16_t adc) {
+    float resistance = R_BALANCE * (1023.0 / adc - 1);
+    float resistance_ratio = resistance / R_REF;
+    float kelvin = 1 / (1 / T_REF + logf(resistance_ratio) / BETA_COEFFICIENT);
+    return kelvin - 273.15;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    uart_putfloat(convert_adc_to_celsius(ADC), 1);
+    uart_putstrln("°C");
+}
+
+static void timer1_init() {
+    TCCR1B |= CTC_MODE | TIMER_PRESCALER_256;
+    OCR1A = F_CPU * PRINT_INTERVAL / 256 - 1;
+    TIMSK1 |= 1 << OCIE1A;
+}
+
+int main() {
+    uart_init();
+    adc_init();
+    timer1_init();
+    sei();
+    while (true) {}
+}
