616d35cc27b3b72f56ccf88393bff0ef443bca0d
Axel Brisse <axel.brisse@gmail.com>
2024-04-25 18:31:13 +0200

    day08/ex00 clean

--- day08/ex00/main.c
+++ day08/ex00/main.c
@@ -1,4 +1,3 @@
-#include "uart.h"
 #include <avr/io.h>
 #include <stdbool.h>
 #include <util/delay.h>
@@ -7,47 +6,43 @@
 #define MOSI (1 << PB3)
 #define SCK (1 << PB5)
 
-void spi_init() {
+static void spi_init_master() {
     DDRB = SS | MOSI | SCK; // no idea why we need SS
-    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0); // enable SPI, master, prescaler 16
+    SPCR = 1 << SPE | 1 << MSTR | 1 << SPR0; // enable SPI, master, prescaler 16
 }
 
-void spi_send(uint8_t data) {
+static void spi_send(uint8_t data) {
     SPDR = data;
-    while (!(SPSR & (1 << SPIF))) {}
+    while (!(SPSR & 1 << SPIF)) {}
 }
 
-void apa102_start() {
+static void apa102_start() {
     spi_send(0x00);
     spi_send(0x00);
     spi_send(0x00);
     spi_send(0x00);
 }
 
-void apa102_color(uint8_t red, uint8_t green, uint8_t blue) {
-    spi_send(0xE4); // 25% brightness
+static void apa102_color(uint8_t red, uint8_t green, uint8_t blue) {
+    spi_send(0xe4); // 25% brightness
     spi_send(blue);
     spi_send(green);
     spi_send(red);
 }
 
-void apa102_end() {
-    spi_send(0xFF);
-    spi_send(0xFF);
-    spi_send(0xFF);
-    spi_send(0xFF);
+static void apa102_end() {
+    spi_send(0xff);
+    spi_send(0xff);
+    spi_send(0xff);
+    spi_send(0xff);
 }
 
 int main() {
-    uart_init();
-    spi_init();
-
-    while (true) {
-        apa102_start();
-        apa102_color(0xff, 0, 0);
-        apa102_color(0, 0, 0);
-        apa102_color(0, 0, 0);
-        apa102_end();
-        _delay_ms(1000);
-    }
+    spi_init_master();
+    apa102_start();
+    apa102_color(0xff, 0, 0);
+    apa102_color(0, 0, 0);
+    apa102_color(0, 0, 0);
+    apa102_end();
+    while (true) {}
 }
