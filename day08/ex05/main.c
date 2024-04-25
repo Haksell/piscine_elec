ddc5ead37cf36dfc52b355ad8e2d7fd7d1bfcc07
Axel Brisse <axel.brisse@gmail.com>
2024-04-25 21:54:57 +0200

    day08/ex05 ok

--- day08/ex05/main.c
+++ day08/ex05/main.c
@@ -4,26 +4,38 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// TODO: remove
-#define RGB_BLACK ((t_rgb){0, 0, 0})
-#define RGB_DODGER_BLUE ((t_rgb){0x1e, 0x90, 0xff})
+#define DEBOUNCE_ITERATIONS 100
+
+uint8_t rgb_index = 0;
+uint8_t led_index = 0;
+
+t_rgb colors[NUM_LEDS];
 
 ISR(ADC_vect) {
-    uint8_t led = ADC * NUM_LEDS / 1024;
-    t_rgb colors[NUM_LEDS];
-    for (uint8_t i = 0; i < NUM_LEDS; ++i) colors[i] = i == led ? RGB_DODGER_BLUE : RGB_BLACK;
+    ((uint8_t*)&colors[led_index])[rgb_index] = ADCH;
     apa102_send_colors(colors);
     ADCSRA |= 1 << ADSC;
 }
 
 static void adc_init() {
-    ADMUX = 1 << REFS0 | PC0;
+    ADMUX = 1 << REFS0 | 1 << ADLAR | PC0;
     ADCSRA = 1 << ADPS2 | 1 << ADPS1 | 1 << ADPS0 | 1 << ADIE | 1 << ADEN | 1 << ADSC;
+}
+
+static bool is_pressed(uint8_t pin, int8_t* debounce) {
+    *debounce = (PIND >> pin) & 1 ? DEBOUNCE_ITERATIONS : *debounce - (*debounce >= 0);
+    return *debounce == 0;
 }
 
 int main() {
     adc_init();
     apa102_init();
     sei();
-    while (true) {}
+    DDRD &= ~(1 << PD2 | 1 << PD4);
+    int8_t sw1 = -1, sw2 = -1;
+    apa102_send_colors(colors);
+    while (true) {
+        if (is_pressed(PD2, &sw1)) rgb_index = (rgb_index + 1) % 3;
+        if (is_pressed(PD4, &sw2)) led_index = (led_index + 1) % NUM_LEDS;
+    }
 }
