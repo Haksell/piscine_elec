7abf2793c09213c79c9422898d1c4fa471222698
Axel Brisse <axel.brisse@gmail.com>
2024-04-26 05:45:17 +0200

    segments move

--- day09/ex02/main.c
+++ day09/ex02/main.c
@@ -14,21 +14,16 @@ static void pca9555_write(uint8_t reg, uint8_t data0, uint8_t data1) {
     i2c_write(reg, true);
     i2c_write(data0, true);
     i2c_write(data1, true);
-    i2c_write(data0, true);
-    i2c_write(data1, true);
     i2c_stop();
 }
 
-/*
-00 00 00 00 LEDS ON
-00 00 00 FF EVERYTHING ON
-*/
-
 int main() {
     i2c_init();
-    pca9555_write(PCA9555_CONFIG_REG, 0x0f, 0);
+    pca9555_write(PCA9555_CONFIG_REG, 0x0f, 0x00);
+    uint8_t n = 0;
     while (true) {
-        pca9555_write(PCA9555_OUTPUT_REG, 0, 0xff);
-        _delay_ms(500);
+        pca9555_write(PCA9555_OUTPUT_REG, 0, n);
+        ++n;
+        _delay_ms(50);
     }
 }
