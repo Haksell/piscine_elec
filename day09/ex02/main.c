32eb1d6bb7d9dc9f1c719a0cf85388e2d83a9737
Axel Brisse <axel.brisse@gmail.com>
2024-04-26 05:32:03 +0200

    all segments on but garbage code

---
+++ day09/ex02/main.c
@@ -0,0 +1,34 @@
+#include "i2c.h"
+#include <avr/io.h>
+#include <stdbool.h>
+#include <util/delay.h> // TODO
+
+#define PCA9555_ADDRESS 0x20
+
+#define PCA9555_OUTPUT_REG 0x02
+#define PCA9555_CONFIG_REG 0x06
+
+static void pca9555_write(uint8_t reg, uint8_t data0, uint8_t data1) {
+    i2c_start();
+    i2c_write(PCA9555_ADDRESS << 1, true);
+    i2c_write(reg, true);
+    i2c_write(data0, true);
+    i2c_write(data1, true);
+    i2c_write(data0, true);
+    i2c_write(data1, true);
+    i2c_stop();
+}
+
+/*
+00 00 00 00 LEDS ON
+00 00 00 FF EVERYTHING ON
+*/
+
+int main() {
+    i2c_init();
+    pca9555_write(PCA9555_CONFIG_REG, 0x0f, 0);
+    while (true) {
+        pca9555_write(PCA9555_OUTPUT_REG, 0, 0xff);
+        _delay_ms(500);
+    }
+}
