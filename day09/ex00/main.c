cc5dc8c064d73c2d9e45b4521a2587ac09046a7c
Axel Brisse <axel.brisse@gmail.com>
2024-04-26 03:44:48 +0200

    things have been understood

--- day09/ex00/main.c
+++ day09/ex00/main.c
@@ -6,14 +6,14 @@
 #define PCA9555_ADDRESS 0x20
 #define PORT0_OUTPUT_REG 0x02
 #define PORT0_CONFIG_REG 0x06
-#define LED_D9 0xf7
+#define LED_D9 (1 << 3)
 
 int main() {
     bool toggle_on = true;
     i2c_init();
-    i2c_send_cmd2(PCA9555_ADDRESS, PORT0_CONFIG_REG, LED_D9);
+    i2c_send_cmd2(PCA9555_ADDRESS, PORT0_CONFIG_REG, ~LED_D9);
     while (true) {
-        i2c_send_cmd2(PCA9555_ADDRESS, PORT0_OUTPUT_REG, toggle_on ? LED_D9 : ~LED_D9);
+        i2c_send_cmd2(PCA9555_ADDRESS, PORT0_OUTPUT_REG, toggle_on ? ~LED_D9 : LED_D9);
         toggle_on = !toggle_on;
         _delay_ms(500);
     }
