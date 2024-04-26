d37baa131a357332cbc5b918fe9ab2d8e2fda156
Axel Brisse <axel.brisse@gmail.com>
2024-04-26 05:53:41 +0200

    day09/ex02 ok

--- day09/ex02/main.c
+++ day09/ex02/main.c
@@ -1,12 +1,15 @@
 #include "i2c.h"
 #include <avr/io.h>
 #include <stdbool.h>
-#include <util/delay.h> // TODO
 
 #define PCA9555_ADDRESS 0x20
 
 #define PCA9555_OUTPUT_REG 0x02
 #define PCA9555_CONFIG_REG 0x06
+
+#define DIGIT2 0b1011011
+
+#define LCD4 (1 << 7)
 
 static void pca9555_write(uint8_t reg, uint8_t data0, uint8_t data1) {
     i2c_start();
@@ -19,11 +22,6 @@ static void pca9555_write(uint8_t reg, uint8_t data0, uint8_t data1) {
 
 int main() {
     i2c_init();
-    pca9555_write(PCA9555_CONFIG_REG, 0x0f, 0x00);
-    uint8_t n = 0;
-    while (true) {
-        pca9555_write(PCA9555_OUTPUT_REG, 0, n);
-        ++n;
-        _delay_ms(50);
-    }
+    pca9555_write(PCA9555_CONFIG_REG, (uint8_t)~LCD4, 0x00);
+    pca9555_write(PCA9555_OUTPUT_REG, 0, DIGIT2);
 }
