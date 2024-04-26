5893ac20b88374cae4190c5b6c6bf22cf37ef307
Axel Brisse <axel.brisse@gmail.com>
2024-04-26 04:18:36 +0200

    pca9555_write

--- day09/ex00/main.c
+++ day09/ex00/main.c
@@ -4,23 +4,14 @@
 #include <util/delay.h>
 
 #define PCA9555_ADDRESS 0x20
-#define PCA9555_OUTPUT_REF 0x02
-#define PCA9555_CONFIG_REF 0x06
+#define PCA9555_OUTPUT_REG 0x02
+#define PCA9555_CONFIG_REG 0x06
 #define LED_D9 (1 << 3)
 
-static void pca9555_write_config(uint8_t data0, uint8_t data1) {
+static void pca9555_write(uint8_t reg, uint8_t data0, uint8_t data1) {
     i2c_start();
     i2c_write(PCA9555_ADDRESS << 1, true);
-    i2c_write(PCA9555_CONFIG_REF, true);
-    i2c_write(data0, true);
-    i2c_write(data1, true);
-    i2c_stop();
-}
-
-static void pca9555_write_output(uint8_t data0, uint8_t data1) {
-    i2c_start();
-    i2c_write(PCA9555_ADDRESS << 1, true);
-    i2c_write(PCA9555_OUTPUT_REF, true);
+    i2c_write(reg, true);
     i2c_write(data0, true);
     i2c_write(data1, true);
     i2c_stop();
@@ -29,9 +20,9 @@ static void pca9555_write_output(uint8_t data0, uint8_t data1) {
 int main() {
     bool toggle_on = true;
     i2c_init();
-    pca9555_write_config(~LED_D9, 0);
+    pca9555_write(PCA9555_CONFIG_REG, ~LED_D9, 0);
     while (true) {
-        pca9555_write_output(toggle_on ? ~LED_D9 : LED_D9, 0);
+        pca9555_write(PCA9555_OUTPUT_REG, toggle_on ? ~LED_D9 : LED_D9, 0);
         toggle_on = !toggle_on;
         _delay_ms(500);
     }
